43be80f887d5dedd09b4b18cbee1d3e61ddade94
andreedetun <45624189+andreedetun@users.noreply.github.com>
2018-12-14 20:36:27 +0100

    Add files via upload

---
+++ Advent Of Code 2018/Day 1/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int freq = 0;
+        int lastFreq = 0;
+        bool hit = false;
+        try
+        {
+            string text;
+            var fileStream = new FileStream(@"c:\test\input.txt", FileMode.Open, FileAccess.Read);
+            using (var streamReader = new StreamReader(fileStream, Encoding.UTF8))
+            {
+                Console.WriteLine("File loaded!");
+                Thread.Sleep(500);
+                Console.Clear();
+
+                String line;
+                while ((line = streamReader.ReadLine()) != null)
+                {
+                    freq += Int32.Parse(line);
+                    freq = lastFreq;
+                }
+
+                if(lastFreq == freq && !hit)
+                {
+                    Console.WriteLine("Dupplicate: " + lastFreq);
+                    hit = true;
+                }
+                
+                Console.WriteLine("Frequency: " + freq);
+            }
+        }
+        catch (Exception e) // simpel hantering av errors
+        {
+            Console.WriteLine("Error!");
+        }
+
+        Thread.Sleep(10000000);
+    }
+}
+
